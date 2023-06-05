0b3775ef0d820a2f82034178ec6bb0b3edd0ddc0
Caitlyn Larson <124397489+larsocai@users.noreply.github.com>
2023-06-05 09:04:21 -0700

    Initial Project Push

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Account from './Account';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Deposit">
+      <Account /></div>
+    </div>
+  );
+}
+
+export default App;
+
